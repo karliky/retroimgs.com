e78ddc76b9a0d2926f46b2bdcca13901dbe15bea
andydawson76 <andydawson76@8a91893a-5284-11de-971b-8d1939f35316>
2009-06-06 11:20:30 +0000

    updating props

--- trunk/app/config/sql/sessions.php
+++ trunk/app/config/sql/sessions.php
@@ -1,5 +1,5 @@
 <?php
-/* SVN FILE: $Id$ */
+/* SVN FILE: $Id: sessions.php 7805 2008-10-30 17:30:26Z AD7six $ */
 /*Sessions schema generated on: 2007-11-25 07:11:54 : 1196004714*/
 /**
  * This is Sessions Schema file
@@ -20,9 +20,9 @@
  * @package       cake
  * @subpackage    cake.app.config.sql
  * @since         CakePHP(tm) v 0.2.9
- * @version       $Revision$
- * @modifiedby    $LastChangedBy$
- * @lastmodified  $Date$
+ * @version       $Revision: 7805 $
+ * @modifiedby    $LastChangedBy: AD7six $
+ * @lastmodified  $Date: 2008-10-30 18:30:26 +0100 (Thu, 30 Oct 2008) $
  * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
  */
 /*
