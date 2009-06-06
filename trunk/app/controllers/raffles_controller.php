1e4a170db21ff795b92b1fcd52dee7f7bf5b946e
jorgealvaro <jorgealvaro@8a91893a-5284-11de-971b-8d1939f35316>
2009-06-06 17:59:11 +0000

    Added Raffles model

--- trunk/app/controllers/raffles_controller.php
+++ trunk/app/controllers/raffles_controller.php
@@ -3,5 +3,34 @@ class RafflesController extends AppController {
 	var $scaffold = array('index', 'add', 'edit', 'delete');
 
 	var $name = 'Raffles';
+
+	//function index() {
+		//$this->set('raffles', $this->Raffle->find('all'));
+	//}
+
+	function add() {
+		
+		// TODO: check admin role
+
+		if (!empty($this->data)) {
+			if ($this->Raffle->save($this->data)) {
+				
+				// Add tickets
+				$aTickets = Array();
+				for($i=0; $i<$this->data["Raffle"]["tickets_count"]; $i++) {
+					$aTickets[$i] =Array("number" => $i, "raffle" => $this->Raffle->id);
+				}
+				$this->Raffle->Ticket->saveAll($aTickets);
+//print_r($this->Raffle->Ticket);
+//print_r($this->data);
+//echo "    $i<br/>";
+//echo $this->Raffle->data["tickets_count"];
+//die();
+				
+				$this->Session->setFlash('Your post has been saved.');
+				$this->redirect(array('action' => 'index'));
+			}
+		}
+	}
 }
 ?>
