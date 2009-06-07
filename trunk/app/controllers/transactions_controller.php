9d73b690bfe50fd244d886d764a5300ef83e59be
rifalia.com@gmail.com <rifalia.com@gmail.com@8a91893a-5284-11de-971b-8d1939f35316>
2009-06-07 19:38:54 +0000

    Adding transactions

--- trunk/app/controllers/transactions_controller.php
+++ trunk/app/controllers/transactions_controller.php
@@ -29,55 +29,88 @@
  */
 class TransactionsController extends AppController {
 
-/**
- * admin_add method
- *
- * @return void
- * @access public
- */
-	function admin_add() {
-		if ($this->data) {
-			if ($this->Transaction->saveAll($this->data)) {
-				$display = $this->Transaction->display();
-				$this->Session->setFlash(sprintf(__('%1$s "%2$s" added', true), 'Transaction', $display));
-				return $this->_back();
-			} else {
-				$this->data = $this->Transaction->data;
-				$this->Session->setFlash(__('errors in form', true));
-			}
-		}
-		$this->_setSelects();
-		$this->render('admin_edit');
-	}
 
+    function index()
+    {
+        $this->Session->setFlash('Invalid transaction request');
+        $this->_back();
+    }
+
+
+    function add()
+    {
+        $this->set('valid_amounts', $this->Transaction->valid_amounts);
+        $this->_setSelects();
+    }
+
+    function confirm()
+    {
+        if (!empty($_POST)) {
+            $this->data = $this->Transaction->save(array('amount'=>$_POST['amount_1'], 'user_id'=>$this->Auth->user('id')));
+            $this->set('valid_amounts', $this->Transaction->valid_amounts);
+            $this->set('transaction_id', $this->Transaction->id);
+        }else{
+            $this->_back();
+        }
+    }
+
+    function processor_callback($id)
+    {
+        $this->Transaction->read(null, $id);
+        if($this->Transaction->validatesProcessorResponse($_GET)){
+            $this->redirect("success/".$id);
+            return;
+        }
+        $this->redirect('failure/'.$id);
+    }
+
+    function success($id)
+    {
+        $this->Transaction->find('first', array(
+                   'conditions' => array('Transaction.id' => $id, 'transaction_type' => 'confirmed_payment')
+            ));
+        $this->Transaction->read(null, $id);
+        if(!$this->Transaction->commitMe()){
+            $this->redirect('failure/'.$id);
+        }
+    }
+
+    function failure($id)
+    {
+    }
+
+    function cancel($id)
+    {
+
+    }
 /**
  * admin_multi_add method
  *
  * @return void
  * @access public
  */
-	function admin_multi_add() {
-		if ($this->data) {
-			$data = array();
-			foreach ($this->data as $key => $row) {
-				if (!is_numeric($key)) {
-					continue;
-				}
-				$data[$key] = $row;
-			}
-			if ($this->Transaction->saveAll($data, array('validate' => 'first', 'atomic' => false))) {
-				$this->Session->setFlash(sprintf(__('%1$s added', true), $this->name));
-				$this->_back();
-			} else {
-				$this->Session->setFlash(__('Some or all additions did not succeed', true));
-			}
-		} else {
-			$this->data = array(array('Transaction' => $this->Transaction->create()));
-			$this->data[0]['Transaction'][$this->Transaction->primaryKey] = null;
-		}
-		$this->_setSelects();
-		$this->render('admin_multi_edit');
-	}
+    function admin_multi_add() {
+        if ($this->data) {
+            $data = array();
+            foreach ($this->data as $key => $row) {
+                if (!is_numeric($key)) {
+                    continue;
+                }
+                $data[$key] = $row;
+            }
+            if ($this->Transaction->saveAll($data, array('validate' => 'first', 'atomic' => false))) {
+                $this->Session->setFlash(sprintf(__('%1$s added', true), $this->name));
+                $this->_back();
+            } else {
+                $this->Session->setFlash(__('Some or all additions did not succeed', true));
+            }
+        } else {
+            $this->data = array(array('Transaction' => $this->Transaction->create()));
+            $this->data[0]['Transaction'][$this->Transaction->primaryKey] = null;
+        }
+        $this->_setSelects();
+        $this->render('admin_multi_edit');
+    }
 
 /**
  * admin_multi_edit method
@@ -87,28 +120,28 @@ class TransactionsController extends AppController {
  * @return void
  * @access public
  */
-	function admin_multi_edit() {
-		if ($this->data) {
-			$data = array();
-			foreach ($this->data as $key => $row) {
-				if (!is_numeric($key)) {
-					continue;
-				}
-				$data[$key] = $row;
-			}
-			if ($this->Transaction->saveAll($data, array('validate' => 'first'))) {
-				$this->Session->setFlash(sprintf(__('%1$s updated', true), $this->name));
-			} else {
-				$this->Session->setFlash(__('Some or all updates did not succeed', true));
-			}
-			$this->_setSelects();
-		} else {
-			$args = func_get_args();
-			call_user_func_array(array($this, 'admin_index'), $args);
-			array_unshift($this->data, 'dummy');
-			unset($this->data[0]);
-		}
-	}
+    function admin_multi_edit() {
+        if ($this->data) {
+            $data = array();
+            foreach ($this->data as $key => $row) {
+                if (!is_numeric($key)) {
+                    continue;
+                }
+                $data[$key] = $row;
+            }
+            if ($this->Transaction->saveAll($data, array('validate' => 'first'))) {
+                $this->Session->setFlash(sprintf(__('%1$s updated', true), $this->name));
+            } else {
+                $this->Session->setFlash(__('Some or all updates did not succeed', true));
+            }
+            $this->_setSelects();
+        } else {
+            $args = func_get_args();
+            call_user_func_array(array($this, 'admin_index'), $args);
+            array_unshift($this->data, 'dummy');
+            unset($this->data[0]);
+        }
+    }
 
 /**
  * admin_delete method
@@ -117,20 +150,20 @@ class TransactionsController extends AppController {
  * @return void
  * @access public
  */
-	function admin_delete($id) {
-		$this->Transaction->id = $id;
-		if ($this->Transaction->exists()) {
-			$display = $this->Transaction->display($id);
-			if ($this->Transaction->del($id)) {
-				$this->Session->setFlash(sprintf(__('%1$s %2$s "%3$s" deleted', true), 'Transaction', $id, $display));
-			} else {
-				$this->Session->setFlash(sprintf(__('Problem deleting %1$s %2$s "%3$s"', true), 'Transaction', $id, $display));
-			}
-		} else {
-			$this->Session->setFlash(sprintf(__('%1$s with id %2$s doesn\'t exist', true), 'Transaction', $id));
-		}
-		return $this->_back();
-	}
+    function admin_delete($id) {
+        $this->Transaction->id = $id;
+        if ($this->Transaction->exists()) {
+            $display = $this->Transaction->display($id);
+            if ($this->Transaction->del($id)) {
+                $this->Session->setFlash(sprintf(__('%1$s %2$s "%3$s" deleted', true), 'Transaction', $id, $display));
+            } else {
+                $this->Session->setFlash(sprintf(__('Problem deleting %1$s %2$s "%3$s"', true), 'Transaction', $id, $display));
+            }
+        } else {
+            $this->Session->setFlash(sprintf(__('%1$s with id %2$s doesn\'t exist', true), 'Transaction', $id));
+        }
+        return $this->_back();
+    }
 
 /**
  * admin_edit method
@@ -139,21 +172,21 @@ class TransactionsController extends AppController {
  * @return void
  * @access public
  */
-	function admin_edit($id) {
-		if ($this->data) {
-			if ($this->Transaction->saveAll($this->data)) {
-				$display = $this->Transaction->display();
-				$this->Session->setFlash(sprintf(__('%1$s "%2$s" updated', true), 'Transaction', $display));
-				return $this->_back();
-			} else {
-				$this->data = $this->Transaction->data;
-				$this->Session->setFlash(__('errors in form', true));
-			}
-		} else {
-			$this->data = $this->Transaction->read(null, $id);
-		}
-		$this->_setSelects();
-	}
+    function admin_edit($id) {
+        if ($this->data) {
+            if ($this->Transaction->saveAll($this->data)) {
+                $display = $this->Transaction->display();
+                $this->Session->setFlash(sprintf(__('%1$s "%2$s" updated', true), 'Transaction', $display));
+                return $this->_back();
+            } else {
+                $this->data = $this->Transaction->data;
+                $this->Session->setFlash(__('errors in form', true));
+            }
+        } else {
+            $this->data = $this->Transaction->read(null, $id);
+        }
+        $this->_setSelects();
+    }
 
 /**
  * admin_view method
@@ -162,13 +195,13 @@ class TransactionsController extends AppController {
  * @return void
  * @access public
  */
-	function admin_view($id) {
-		$this->data = $this->Transaction->read(null, $id);
-		if(!$this->data) {
-			$this->Session->setFlash(sprintf(__('Invalid %1$s', true), 'Transaction'));
-			return $this->_back();
-		}
-	}
+    function admin_view($id) {
+        $this->data = $this->Transaction->read(null, $id);
+        if(!$this->data) {
+            $this->Session->setFlash(sprintf(__('Invalid %1$s', true), 'Transaction'));
+            return $this->_back();
+        }
+    }
 
 /**
  * admin_index method
@@ -179,20 +212,20 @@ class TransactionsController extends AppController {
  * @return void
  * @access public
  */
-	function admin_index() {
-		if (isset($this->SwissArmy)) {
-			$conditions = $this->SwissArmy->parseSearchFilter();
-		} else {
-			$conditions = array();
-		}
-		if ($conditions) {
-			$this->set('filters', $this->Transaction->searchFilterFields());
-			$this->set('addFilter', true);
-		}
-		$this->Transaction->recursive = 1;
-		$this->data = $this->paginate($conditions);
-		$this->_setSelects();
-	}
+    function admin_index() {
+        if (isset($this->SwissArmy)) {
+            $conditions = $this->SwissArmy->parseSearchFilter();
+        } else {
+            $conditions = array();
+        }
+        if ($conditions) {
+            $this->set('filters', $this->Transaction->searchFilterFields());
+            $this->set('addFilter', true);
+        }
+        $this->Transaction->recursive = 1;
+        $this->data = $this->paginate($conditions);
+        $this->_setSelects();
+    }
 
 /**
  * admin_search method
@@ -201,25 +234,25 @@ class TransactionsController extends AppController {
  * @return void
  * @access public
  */
-	function admin_search($term = null) {
-		if ($this->data) {
-			$term = trim($this->data['Transaction']['query']);
-			$url = array(urlencode($term));
-			if ($this->data['Transaction']['extended']) {
-				$url['extended'] = true;
-			}
-			$this->redirect($url);
-		}
-		$request = $_SERVER['REQUEST_URI'];
-		$term = trim(str_replace(Router::url(array()), '', $request), '/');
-		if (!$term) {
-			$this->redirect(array('action' => 'index'));
-		}
-		$conditions = $this->Transaction->searchConditions($term, isset($this->passedArgs['extended']));
-		$this->Session->setFlash(sprintf(__('All transactions matching the term "%1$s"', true),
-			Inflector::humanize(Inflector::underscore($this->name)), htmlspecialchars($term)));
-		$this->data = $this->paginate($conditions);
-		$this->_setSelects();
-		$this->render('admin_index');
-	}
+    function admin_search($term = null) {
+        if ($this->data) {
+            $term = trim($this->data['Transaction']['query']);
+            $url = array(urlencode($term));
+            if ($this->data['Transaction']['extended']) {
+                $url['extended'] = true;
+            }
+            $this->redirect($url);
+        }
+        $request = $_SERVER['REQUEST_URI'];
+        $term = trim(str_replace(Router::url(array()), '', $request), '/');
+        if (!$term) {
+            $this->redirect(array('action' => 'index'));
+        }
+        $conditions = $this->Transaction->searchConditions($term, isset($this->passedArgs['extended']));
+        $this->Session->setFlash(sprintf(__('All transactions matching the term "%1$s"', true),
+                Inflector::humanize(Inflector::underscore($this->name)), htmlspecialchars($term)));
+        $this->data = $this->paginate($conditions);
+        $this->_setSelects();
+        $this->render('admin_index');
+    }
 }
---
+++ trunk/app/views/transactions/cancel.ctp
@@ -0,0 +1,4 @@
+<h1>Transacción cancelada.</h1>
+
+
+
