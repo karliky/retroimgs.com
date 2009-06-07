2f9d5c137756230f21e9a6fd709573de42e52d1f
jorgealvaro <jorgealvaro@8a91893a-5284-11de-971b-8d1939f35316>
2009-06-07 08:35:36 +0000

    Removed unused code

--- app/controllers/raffles_controller.php
+++ app/controllers/raffles_controller.php
@@ -45,30 +45,6 @@ class RafflesController extends AppController {
  */
 	var $name = 'Raffles';
 
-	//function index() {
-		//$this->set('raffles', $this->Raffle->find('all'));
-	//}
-
-	/*function add() {
-
-		// TODO: check admin role
-
-		if (!empty($this->data)) {
-			if ($this->Raffle->save($this->data)) {
-
-				// Add tickets
-				$aTickets = Array();
-				for($i=0; $i<$this->data["Raffle"]["tickets_count"]; $i++) {
-					$aTickets[$i] =Array("number" => $i, "raffle" => $this->Raffle->id);
-				}
-				$this->Raffle->Ticket->saveAll($aTickets);
-
-				$this->Session->setFlash('Your post has been saved.');
-				$this->redirect(array('action' => 'index'));
-			}
-		}
-	}*/
-
 /**
  * winner method
  *
