8ebbf645b7ef82487ef24f76ea251b9f4d761ce1
andydawson76 <andydawson76@8a91893a-5284-11de-971b-8d1939f35316>
2009-06-18 00:27:07 +0000

    allowing more media sizes, used in admin screens

--- branches/development/app/config/media.php
+++ branches/development/app/config/media.php
@@ -1,6 +1,9 @@
 <?php
 $config['Media'] = array(
-	'large' => array(550, null, array()),
-	'medium' => array(470, null, array()),
+	'tiny' => array(20, 20, array('fit' => 'chop')),
+	'icon' => array(32, 32, array('fit' => 'chop')),
+	'thumb' => array(50, 50, array('fit' => 'chop')),
 	'small' => array(110, null, array()),
+	'medium' => array(470, null, array()),
+	'large' => array(550, null, array()),
 );
